272fd7e606450f48ea7d5e040f88c362280b5ff5
Michael Kipchumba <michaelchumba09@gmail.com>
2019-01-11 14:46:39 +0300

    add forms module to root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDetailsService } from './quote-details.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,10 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [QuoteDetailsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
