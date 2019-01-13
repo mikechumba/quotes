11aecd2c0a02feed7d73fc019623a0e515ccc9d0
Michael Kipchumba <michaelchumba09@gmail.com>
2019-01-13 15:48:59 +0300

    validate form

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Quotes } from './../quotes/quotes';
+import { QuoteSamples } from './../quotes/sample-quotes';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-quote-form',
@@ -9,9 +13,34 @@ export class QuoteFormComponent implements OnInit {
 
   constructor() { }
 
+  quotesForm = new FormGroup ({
+    name: new FormControl('',
+      [Validators.required]
+    ),
+    author: new FormControl('',
+      [Validators.required]
+      ),
+    quote: new FormControl('',
+      [Validators.required]
+    )
+  });
+
   ngOnInit() {
   }
 
-  
+  get name() {
+    return this.quotesForm.get('name');
+  }
 
+  get author() {
+    return this.quotesForm.get('author');
+  }
+
+  get quote() {
+    return this.quotesForm.get('quote');
+  }
+
+  publish() {
+    console.log(this.quotesForm.value);
+  }
 }
